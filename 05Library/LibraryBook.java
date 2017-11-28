a114097cead3e1ec796252af908966637be39cd7
Yuxuanzou <yzou2@stuy.edu>
2017-11-28 15:31:26 -0500

    Fixed the errors on the getter and setter method

--- 05Library/LibraryBook.java
+++ 05Library/LibraryBook.java
@@ -8,29 +8,17 @@ public abstract class LibraryBook extends Book implements Comparable<LibraryBook
 	this.callNumber = callNumber;
     }
 
-    public getCallNumber(){
+    public String getCallNumber(){
 	return callNumber;
     }
 
-    public setCallNumber(String words){
+    public void setCallNumber(String words){
 	callNumber = words; 
     }
-
-    public abstract void checkout(String patron, String due){
-	
-    }
-
-    public abstract void returned(){
-	
-    }
-
-    public abstract String circulationStatus(){
-	
-    }
-
-    public String toString(){
-	return (super.toString() + circulationStatus + callNumber);
-    }
+    
+    public abstract void checkout(String patron, String due);
+    public abstract void returned();
+    public abstract String circulationStatus();
 }
 
 							  
