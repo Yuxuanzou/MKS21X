ee8c6587c5169d9996e7718f7d6eb76bcbf474a8
Yuxuanzou <yzou2@stuy.edu>
2017-12-06 20:08:21 -0700

    Finished InsertionSort and tested it on a driver

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -5,42 +5,69 @@ public class Sorts{
         return "10.Zou.Yuxuan";
     }
     
+    public static boolean isSorted(int[]ary){                                       
+        for(int i = 0; i < ary.length - 1 ; i++){                                   
+            if(ary[i] > ary[i+1]){                                                  
+                return false;                                                       
+            }                                                                       
+        }                                                                           
+        return true;                                                                
+    }                                                                               
+                                          
+
+    public static void bogoSort(int[] ary){                                         
+        while(!isSorted(ary)){                                                      
+            for(int i = 0 ; i < ary.length; i++){                                   
+                int temp = ary[i];                                                  
+                int newSpot = (int)(Math.random()*ary.length);                      
+                ary[i] = ary[newSpot];                                              
+                ary[newSpot] = temp;                                                
+            }                                                                       
+        }                                                                           
+    }                                                                               
+                                                                                    
+    private static void swap(int[]ary,int a, int b){                                
+        int c =ary[a];                                                              
+        ary[a] = ary[b];                                                            
+        ary[b] = c;                                                                 
+    }
+    
     public static void selectionSort(int [] data){
         int num = 0;
-        int currentIndex = 0;
+        int index = 0;
         for (int i = 0; i < data.length; i++){
             num = data[i];
-            currentIndex = i;
+            index = i;
             for (int c = i; c < data.length; c++){
                 if (data[c] < num){
-                    currentIndex = c;
+                    index = c;
                     num = data[c];
                 }
             }
-            data[currentIndex] = data[i];  
+            data[index] = data[i];  
             data[i] = num;
         }
     }
 
-    private static boolean helpMe(int i, int k){
-	return i < k;
-    }
-
     public static void InsertionSort(int [] data){
-	int temp;
-	for (int i = 1;i < data.length;i++){
-	    while (helpMe(data[tick],data[tick - 1])){
-		int tick = i; 
-		temp = data[tick - 1];
-		data[tick - 1] = data[tick];
-		data[tick] = temp;
-	    }
-	}
+       int index;
+       int num;
+	   for (int i = 0;i < data.length;i++){
+           index = i;
+           num = data[i];
+           for (int c = index; c > 0; c--){
+                if (num < data[c-1]){
+                    data[index] = data[c - 1];
+                    index = index - 1;
+                    data[index] = num;
+                }
+           }
+       }
     }
 
-    public static void main(String[]args){
-	int [] a = {123,213,41,33,23,55,313,1323};
-	InsertionSort(a);
-	System.out.println(Arrays.toString(a));
-    } 
+    //public static void main(String[]args){
+	//int [] a = {123,213,41,33,23,55,313,1323};
+	//Sorts.InsertionSort(a);
+	//System.out.println(Arrays.toString(a));
+    //} 
 }
